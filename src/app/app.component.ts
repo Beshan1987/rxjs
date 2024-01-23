16d50bca7fbf40bc14f6cd8426c95c5500adbc60
User <user@MacBook-Pro.local>
2024-01-23 23:16:51 +0300

    feat: add input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {
+  Observable,
+  Subscriber,
+  debounce,
+  debounceTime,
+  distinctUntilChanged,
+  from,
+  map,
+  of,
+  reduce,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'rxjs';
+  search$ = new Observable<string>((Subscriber) => {
+    const search = document.getElementById('search');
+
+    if (!search) {
+      Subscriber.error();
+    }
+    search?.addEventListener('input', (event) => {
+      Subscriber.next((event.target as HTMLInputElement).value);
+    });
+  });
+
+  constructor() {}
+
+  input() {
+    this.search$
+      .pipe(debounceTime(1000), distinctUntilChanged())
+      .subscribe((e) => console.log(e));
+  }
 }
